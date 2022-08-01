86bcb8e9a57deb03e5e0c90251e23ef9d6aab174
Caio Vinícius <caio1525pereira@gmail.com>
2022-08-01 20:22:16 -0300

    feat: Sistema de rotas

---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import { DefaultLayout } from "./layouts/DefaultLayout";
+
+import { Home } from "./pages/Home";
+
+function Router() {
+  return (
+    <Routes>
+      <Route element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export { Router };
